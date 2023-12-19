e60ca4be8fed58ae5baae885ec9e12b0d0afee18
Allan Ferreira <allanferreira2001@hotmail.com>
2023-12-19 09:11:00 -0300

    implementation makeFile

--- lib/features/home/presentation/controller/login_controller.dart
+++ lib/features/home/presentation/controller/login_controller.dart
@@ -1,2 +1,9 @@
-class LoginController {
+import 'package:mobx/mobx.dart';
+
+part 'login_controller.g.dart';
+
+class LoginController = _LoginController with _$LoginController;
+
+abstract class _LoginController with Store {
+  
 }
